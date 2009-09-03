e1ef4b41f9b9223948400c9944838863b485f20f
Orlando Vazquez <ovazquez@gmail.com>
2009-09-03 00:03:38 -0700

    get the BackExceptions and EndExceptions working

--- dialogue.py
+++ dialogue.py
@@ -11,45 +11,104 @@ import itertools
 class EndException(Exception):
     pass
 
+class ResponseException(Exception):
+    pass
+
+class BackException(Exception):
+    pass
+
 class DialogueEngine(object):
     def __init__(self, *args, **kwargs):
-        if kwargs.get("tree"):
-            callbacks = args[0]
-            self.tree = kwargs.get('tree')
-        else:
-            (filename, callbacks) = args
+        """A very simple dialogue engine for a game.
+           d = DialogueEngine(tree, callbacks)
+           d = DialogueEngine('screenplay.yaml', callbacks)
+           d.run() -> runs script until next responses prompt and then fires calls 
+                      the respsonses callback
+           d.reply(1) -> replies with the desired response"""
+
+        (obj, callbacks) = args
+        if isinstance(obj, dict):
+            self.tree = obj
+        elif instance(obj, str):
             self.tree = yaml.load(file(filename))
+
         self.callbacks = callbacks
 
     def run(self):
         start_section = self.tree['START']
+        self.section_stack = []
+
         try:
             self.run_section(start_section)
         except EndException:
             # we stopped talking to the NPC
             return
+        except ResponseException, e:
+            return e
+        except BackException, e:
+            self.section_stack.pop(-1)
+            try:
+                self.run_section(self.section_stack[-1])
+                return e
+            except ResponseException, e:
+                return e
 
     def get_section(self, section_name):
         return self.tree['SECTIONS'][section_name]
 
-    def run_section(self, section_name):
+    def reply(self, choice):
+        try:
+            self.run_section(self.section_stack[-1], choice)
+        except ResponseException, e:
+            print "got response exception", e
+            return e
+        except BackException, e:
+            self.section_stack.pop(-1)
+            try:
+                print "Trying to run ", self.section_stack[-1]
+                self.run_section(self.section_stack[-1])
+                return e
+            except ResponseException, e:
+                return e
+
+    def run_section(self, section_name, choice=None):
         tree = self.tree
+        print "run_section", section_name, self.section_stack
+
+        if choice is None:
+            self.section_stack.append(section_name)
+
+        if len(self.section_stack) > 1:
+            if self.section_stack[-1] == self.section_stack[-2]:
+                self.section_stack.pop(-1)
+
         for command in itertools.cycle(self.get_section(section_name)):
+            # a simple string command
             if isinstance(command, str):
                 if command == "end":
                     # indicate we"d like to stop talking
                     raise EndException
                 elif command == "back":
-                    return
+                    raise BackException()
                 else:
                     raise Exception("Unknown command %s" % (command,))
+            # a hash command
             elif isinstance(command, dict):
                 if command.get("say"):
-                    self.callbacks["say"](command["say"])
+                    if choice is None:
+                        self.callbacks["say"](command["say"])
                 elif command.get("responses"):
-                    section = self.callbacks["responses"](command.get("responses"))
-                    if section == "back": return
-                    self.run_section(section)
+                    if choice is None:
+                        self.callbacks["responses"](command.get("responses"))
+                        raise ResponseException(command.get("responses"))
+                    else:
+                        section = command.get('responses')[choice][1]
+                        print "section was", section
+                        if section == "back":
+                            raise BackException()
+                        elif section == "end":
+                            raise EndException()
+                        self.run_section(section)
                 elif command.get("start_quest"):
                     self.callbacks["quest"](command.get("start_quest"))
                 else:
--- test_dialogue.py
+++ test_dialogue.py
@@ -11,57 +11,62 @@ class TestDialogue(unittest.TestCase):
             'START': 'main',
             'SECTIONS': {
                 'main': [
-                    { "say": "say1" },
+                    { "say": "Greetings stranger" },
                     { "responses": [
-                        ["response1", "resp1"],
-                        ["response2", "resp2"],
+                        ["Hi, can you tell me where I am?", "friendly"],
+                        ["Watch your words", "aggro"],
                         ["response3", "stop"],
                     ] }
                 ],
-                'resp1': [
-                    { "say": "hello" },
+                'friendly': [
+                    { "say": "You sure are lost" },
                     { "responses": [
-                        ["response4", "resp4"],
-                        ["response5", "back"],
+                        ["Thanks, I know", "thanks"],
+                        ["Wait what did you say before?", "back"],
                     ] }
                 ],
-                'stop': [
-                    'end'    
-                ]
-
+                'thanks': [
+                    { "say": "We haven't seen one of your kind in ages" },
+                    { "responses": [
+                        ["Say the other thing again", "back"],
+                        ["I'm leaving now", "end"],
+                    ] }
+                ],
             }
         }
         # record actions in state
         self.state = { "say": [], "responses": [] }
 
         def say_cb(text):
+            print "npc said", text
             self.state["say"].append(text)
 
         self.replies = ["resp1", "back", "stop"]
 
         def responses_cb(responses):
-            self.state['responses'].append([ response[0] for response in responses ])
-            resp = self.replies.pop(0)
-            return resp
+            pass
 
         callbacks = {
             "say": say_cb,
             "responses": responses_cb
         }
-        self.dialogue = dialogue.DialogueEngine(callbacks, tree=self.tree)
+        self.dialogue = dialogue.DialogueEngine(self.tree, callbacks)
 
     def test_simple(self):
-        self.dialogue.run()
-        self.assertEqual(self.state,
-            {
-                "say": ["say1", "hello", "say1"],
-                "responses": [
-                    ["response1", "response2", "response3"],
-                    ["response4", "response5"],
-                    ["response1", "response2", "response3"],
-                ]
-            }
-        )
+        print self.dialogue.run()
+        # npc says: Greetings stranger
+        print self.dialogue.reply(0)
+        # npc says: You sure are lost
+        print self.dialogue.reply(1)
+        # npc says: Greetings stranger
+        print self.dialogue.reply(0)
+        # npc says: You sure are lost
+        print self.dialogue.reply(1)
+        # npc says: Greetings stranger
+        print self.dialogue.reply(0)
+        # npc says: We haven't seen one of your kind in ages
+        print self.dialogue.reply(0)
+        print self.dialogue.reply(1)
 
 if __name__ == "__main__":
     unittest.main()
